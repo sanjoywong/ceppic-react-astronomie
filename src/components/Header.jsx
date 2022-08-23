aa173708d1b01851bea1507a79e3b7938e91e3e2
unknown <sanjoy.wong@gmail.com>
2022-08-23 18:24:05 +0800

    ajoute Header et Footer dans app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+import React from 'react';
+
+export default function Header({title,texte}) {
+    return (
+        <header className="header">
+          <h1>{title}</h1>
+          <p>{texte}</p>
+        </header>
+      )
+};
